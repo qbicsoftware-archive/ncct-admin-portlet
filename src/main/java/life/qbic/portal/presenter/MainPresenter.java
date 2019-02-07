70f58246d78380c2316d85d84d45f4e66a7a5422
wow-such-code <andreas.friedrich@uni-tuebingen.de>
2019-02-07 12:50:17 +0100

    add file to db

--- src/main/java/life/qbic/portal/presenter/MainPresenter.java
+++ src/main/java/life/qbic/portal/presenter/MainPresenter.java
@@ -1,75 +1,83 @@
 package life.qbic.portal.presenter;
 
 import com.vaadin.ui.VerticalLayout;
+
+import life.qbic.portal.model.Batch;
 import life.qbic.portal.model.DBConfig;
 import life.qbic.portal.model.DBManager;
+import life.qbic.portal.model.Experiment;
 import life.qbic.portal.model.Person;
+import life.qbic.portal.model.Project;
+import life.qbic.portal.model.Vocabulary;
 import life.qbic.portal.utils.ConfigurationManager;
 import life.qbic.portal.utils.ConfigurationManagerFactory;
 import life.qbic.portal.utils.LiferayIndependentConfigurationManager;
 import life.qbic.portal.utils.PortalUtils;
 import life.qbic.portal.view.Overview.ProjectsLayout;
 
+import java.sql.Date;
+import java.sql.SQLException;
+import java.util.Calendar;
 import java.util.List;
 
 public class MainPresenter {
 
-    //TODO in case a project is pressed directly add edit function, but only for admins?
+  // TODO in case a project is pressed directly add edit function, but only for admins?
 
-    private final VerticalLayout canvas;
-    private final ProjectsLayout projectsLayout;
+  private final VerticalLayout canvas;
+  private final ProjectsLayout projectsLayout;
 
-    private final DBManager db;
+  private final DBManager db;
 
-    public MainPresenter(){
-        this.projectsLayout = new ProjectsLayout();
-        this.canvas = new VerticalLayout();
+  public MainPresenter() {
+    this.projectsLayout = new ProjectsLayout();
+    this.canvas = new VerticalLayout();
 
-        this.canvas.addComponent(projectsLayout);
+    this.canvas.addComponent(projectsLayout);
 
-        ConfigurationManager config;
+    ConfigurationManager config;
 
-        if (PortalUtils.isLiferayPortlet()) {
-            config   = ConfigurationManagerFactory.getInstance();
-        } else {
-            LiferayIndependentConfigurationManager.Instance.init("local.properties");
-            config = LiferayIndependentConfigurationManager.Instance;
-        }
-
-        db = new DBManager(new DBConfig(config.getMysqlHost(), config.getMysqlPort(),
-                config.getNCCTMysqlDB(), config.getMysqlUser(), config.getMysqlPass()));
-
-        initDB();
-        addListener();
+    if (PortalUtils.isLiferayPortlet()) {
+      config = ConfigurationManagerFactory.getInstance();
+    } else {
+      LiferayIndependentConfigurationManager.Instance.init("local.properties");
+      config = LiferayIndependentConfigurationManager.Instance;
     }
 
-    private void initDB(){
-        db.initVocabularies();
-    }
+    db = new DBManager(new DBConfig(config.getMysqlHost(), config.getMysqlPort(),
+        config.getNCCTMysqlDB(), config.getMysqlUser(), config.getMysqlPass()));
 
-    private void addListener(){
-        addButtonListener();
-    }
+    initDB();
+    addListener();
+  }
 
-    private void addButtonListener(){
-        this.projectsLayout.getAddNewProjectButton().addClickListener(clickEvent -> {
-            this.canvas.removeAllComponents();
-            FormPresenter formPresenter = new FormPresenter(this);
-            this.canvas.addComponent(formPresenter.getFormLayout());
-        });
-    }
+  private void initDB() {
+    db.initVocabularies();
+  }
 
-    public void displayProjects(){
-        //TODO get existing projects and add them to the form
-        this.canvas.removeAllComponents();
-        this.canvas.addComponent(projectsLayout);
-    }
+  private void addListener() {
+    addButtonListener();
+  }
 
-    public VerticalLayout getCanvas() {
-        return canvas;
-    }
+  private void addButtonListener() {
+    this.projectsLayout.getAddNewProjectButton().addClickListener(clickEvent -> {
+      this.canvas.removeAllComponents();
+      FormPresenter formPresenter = new FormPresenter(this);
+      this.canvas.addComponent(formPresenter.getFormLayout());
+    });
+  }
 
-    public DBManager getDb() {
-        return db;
-    }
+  public void displayProjects() {
+    // TODO get existing projects and add them to the form
+    this.canvas.removeAllComponents();
+    this.canvas.addComponent(projectsLayout);
+  }
+
+  public VerticalLayout getCanvas() {
+    return canvas;
+  }
+
+  public DBManager getDb() {
+    return db;
+  }
 }
